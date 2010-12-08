99f3160eb2ac36bad21bfcb652ef264b5b6d9f57
Takayoshi Kimura <tkimura@redhat.com>
2010-12-08 12:20:59 +0900

    Removed ejb3 project, it's separated into different projects

--- ejb3/src/main/java/jp/programmers/examples/ejb3/sfsb/Hello.java
+++
@@ -1,9 +0,0 @@
-package jp.programmers.examples.ejb3.sfsb;
-
-public interface Hello {
-    public String hello();
-    public String hello(String name);
-    public String getLastMessage();
-    public void exception();
-    public void destroy();
-}
