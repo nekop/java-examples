af6a8b0910293019e0500ee0c31ef50919e1c2b0
Takayoshi Kimura <tkimura@redhat.com>
2010-12-09 12:34:10 +0900

    Added @ManyToMany example

--- jpa/src/test/java/jp/programmers/examples/jpa/CatTest.java
+++ jpa/src/test/java/jp/programmers/examples/jpa/CatTest.java
@@ -3,6 +3,7 @@ package jp.programmers.examples.jpa;
 import javax.persistence.Persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import junit.framework.TestCase;
 
 public class CatTest extends TestCase {
@@ -11,6 +12,10 @@ public class CatTest extends TestCase {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
+        em.createNativeQuery("SET WRITE_DELAY FALSE").executeUpdate();
+        em.getTransaction().commit();
+        
+        em.getTransaction().begin();
         Cat cat = new Cat();
         em.persist(cat);
         em.flush();
@@ -18,6 +23,9 @@ public class CatTest extends TestCase {
         em.close();
         em = emf.createEntityManager();
         System.out.println(em.createQuery("from Cat").getSingleResult());
+        Query q = em.createQuery("select m from MMCat AS m, IN (m.friends) f where f.id = :id");
+        q.setParameter("id", 1);
+        System.out.println(q.getResultList());
         em.close();
     }
 
