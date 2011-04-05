032bf37a8cb0d7d27fd48885b18c9355307e8c1a
Takayoshi Kimura <tkimura@redhat.com>
2011-04-05 10:28:46 +0900

    Add several methods

--- ejb2slsb/src/main/java/jp/programmers/examples/ejb2/slsb/HelloSLSBBean.java
+++ ejb2slsb/src/main/java/jp/programmers/examples/ejb2/slsb/HelloSLSBBean.java
@@ -5,7 +5,6 @@ import java.rmi.RemoteException;
 import javax.ejb.EJBException;
 import javax.ejb.SessionContext;
 
-
 /**
  * @ejb.bean name="HelloSLSB" type="Stateless"
  */
@@ -26,6 +25,48 @@ public class HelloSLSBBean implements javax.ejb.SessionBean {
         System.out.println("HelloSLSB#hello(String)");
         System.out.println("name=" + name);
         return "Hello " + name;
+    }
+
+    /**
+     * @ejb.interface-method view-type="local"
+     */
+    public String helloLocal() {
+        System.out.println("HelloSLSB#helloLocal()");
+        return this.hello("world");
+    }
+
+    /**
+     * @ejb.interface-method view-type="local"
+     */
+    public String helloLocal(String name) {
+        System.out.println("HelloSLSB#helloLocal(String)");
+        System.out.println("name=" + name);
+        return "Hello " + name;
+    }
+
+    /**
+     * @ejb.interface-method view-type="remote"
+     */
+    public Object echo(Object o) {
+        return o;
+    }
+
+    /**
+     * @ejb.interface-method view-type="local"
+     */
+    public Object echoLocal(Object o) {
+        return o;
+    }
+
+    /**
+     * @ejb.interface-method view-type="remote"
+     */
+    public void sleep(int msec) {
+        System.out.println("HelloSLSB#sleep(int)");
+        System.out.println("msec=" + msec);
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException ignore) { }
     }
 
     /**
