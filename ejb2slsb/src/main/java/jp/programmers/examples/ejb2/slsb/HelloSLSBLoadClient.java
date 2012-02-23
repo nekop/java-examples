571d5720517d96598e5cd7224f6fb694d38a6cac
Takayoshi Kimura <tkimura@redhat.com>
2012-02-23 15:21:01 +0900

    Small updates

---
+++ ejb2slsb/src/main/java/jp/programmers/examples/ejb2/slsb/HelloSLSBLoadClient.java
@@ -0,0 +1,29 @@
+package jp.programmers.examples.ejb2.slsb;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class HelloSLSBLoadClient {
+
+    public static void main(final String... args) throws Exception {
+        // Confirm single request works before load testing
+        HelloSLSBClient.main(args);
+
+        ExecutorService ex = Executors.newCachedThreadPool();
+        Runnable r = new Runnable() {
+                public void run() {
+                    try {
+                        HelloSLSBClient.main(args);
+                    } catch (Exception ignore) {
+                    }
+                }
+            };
+        for (int i = 0; i < 4000; i++) {
+            ex.execute(r);
+        }
+        ex.awaitTermination(60, TimeUnit.SECONDS);
+        ex.shutdown();
+    }
+
+}
