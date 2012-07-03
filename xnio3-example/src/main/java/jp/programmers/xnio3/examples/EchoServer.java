c3d1a05e3e6698b2e092255f1c697cafa65eb75b
Takayoshi Kimura <tkimura@redhat.com>
2012-07-03 18:05:37 +0900

    Add xnio3-example

---
+++ xnio3-example/src/main/java/jp/programmers/xnio3/examples/EchoServer.java
@@ -0,0 +1,9 @@
+package jp.programmers.xnio3.examples;
+
+import org.xnio.Xnio;
+
+public class EchoServer {
+    public static void main(String[] args) throws Exception {
+        Xnio xnio = Xnio.getInstance();
+    }
+}
