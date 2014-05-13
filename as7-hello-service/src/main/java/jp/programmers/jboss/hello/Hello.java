09260119246cde92bd0882d4edc0b834ad181963
Takayoshi Kimura <tkimura@redhat.com>
2014-05-13 10:34:06 +0900

    Add create and destroy methods

--- as7-hello-service/src/main/java/jp/programmers/jboss/hello/Hello.java
+++ as7-hello-service/src/main/java/jp/programmers/jboss/hello/Hello.java
@@ -1,10 +1,16 @@
 package jp.programmers.jboss.hello;
 
 public class Hello implements HelloMBean {
+    public void create() throws Exception {
+        System.out.println("Hello.create()");
+    }
     public void start() throws Exception {
         System.out.println("Hello.start()");
     }
     public void stop() throws Exception {
         System.out.println("Hello.stop()");
+    }
+    public void destroy() throws Exception {
+        System.out.println("Hello.destroy()");
     }
 }
--- as7-hello-service/src/main/java/jp/programmers/jboss/hello/HelloMBean.java
+++ as7-hello-service/src/main/java/jp/programmers/jboss/hello/HelloMBean.java
@@ -1,6 +1,8 @@
 package jp.programmers.jboss.hello;
 
 public interface HelloMBean {
+    void create() throws Exception;
     void start() throws Exception;
     void stop() throws Exception;
+    void destroy() throws Exception;
 }
