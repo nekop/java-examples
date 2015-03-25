254424fa78618142ad1cd8f9dc9019e2496288a5
Takayoshi Kimura <takayoshi@gmail.com>
2015-03-25 14:53:36 +0900

    Add simple example

--- rhgss/jdg-standalone/src/main/java/com/redhat/gss/example/JDGStandalone.java
+++ rhgss/jdg-standalone/src/main/java/com/redhat/gss/example/JDGStandalone.java
@@ -8,10 +8,38 @@
  */
 package com.redhat.gss.example;
 
+import org.infinispan.Cache;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.CacheMode;
+
 public class JDGStandalone {
 
-    public static void main(String[] args) {
-        // todo: implement Infinispan stuff
+    public static void main(String[] args) throws Exception {
+        simple();
     }
 
+    public static void simple() throws Exception {
+        GlobalConfiguration globalConfig =
+            new GlobalConfigurationBuilder()
+            .globalJmxStatistics().allowDuplicateDomains(true)
+            .transport()
+            .defaultTransport()
+            .build();
+        Configuration defaultCacheConfig =
+            new ConfigurationBuilder()
+            .clustering()
+            .cacheMode(CacheMode.DIST_ASYNC)
+            .build();
+        DefaultCacheManager cm = new DefaultCacheManager(globalConfig, defaultCacheConfig);
+        Cache cache = cm.getCache();
+        System.out.println("sleep 20 sec");
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException ignore) { }
+        cm.stop();
+    }
 } 
